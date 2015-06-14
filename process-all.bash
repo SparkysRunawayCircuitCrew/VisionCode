6ae3088f8ab923dba117fca8a7506f8c88485b50
Andrew Bass <andybass009@gmail.com>
2015-06-14 16:55:24 -0400

    Print out files that are missing stanchions

--- process-all.bash
+++ process-all.bash
@@ -4,6 +4,7 @@
 
 declare -i found=0;
 declare -i missed=0;
+declare missing_files="";
 
 for f in $(find . -name "*.png"); do
   echo -e "\nProcessing: ${f}  ";
@@ -11,6 +12,7 @@ for f in $(find . -name "*.png"); do
     found=$((found + 1));
   else
     missed=$((missed + 1));
+    missing_files="${missing_files} $f";
   fi
 done
 
@@ -20,5 +22,9 @@ echo "
 
 Found stanchion in ${found} of the ${total} images (missed ${missed})
 "
+
+for f in ${missing_files}; do
+    echo "  $f";
+done
 
 exit ${missed};
